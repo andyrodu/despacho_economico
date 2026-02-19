674766e078f8aacc107ee5dc8d75ad3d367a2c95
Admin <ADMIN@MacBookProdeAndrea.local>
2026-02-18 23:06:56 -0600

    Semana 1: skeleton funcionando

---
+++ app/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Simulador Despacho Económico MX", layout="wide")
+
+st.title("Simulador de Despacho Económico (MX)")
+st.write("Skeleton - Semana 1")
+
+st.info("""
+Este es el esqueleto inicial del simulador.
+Aquí iremos construyendo el modelo paso a paso.
+""")
